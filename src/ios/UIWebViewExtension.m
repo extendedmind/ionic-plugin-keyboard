11910e871efa0462b55470654b7ee0f9ee214947
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-03-23 09:45:52 +0200

    Attempt a clever trick, hopefully very temporary.

--- src/ios/UIWebViewExtension.m
+++ src/ios/UIWebViewExtension.m
@@ -5,50 +5,51 @@
 //Credit: https://gist.github.com/bjhomer/2048571
 //Also: http://stackoverflow.com/a/23398487/1091751
 @implementation UIWebView (HackishAccessoryHiding)
- 
-static const char * const hackishFixClassName = "UIWebBrowserViewMinusAccessoryView";
+
+static char * hackishFixClassName = Nil;
 static Class hackishFixClass = Nil;
- 
+
 - (UIView *)hackishlyFoundBrowserView {
     UIScrollView *scrollView = self.scrollView;
-    
+    hackishFixClassName = [[@[@"UI",@"Web",@"Browser",@"View",@"Minus",@"Accessory",@"View"] componentsJoinedByString:@""] UTF8String];
+    NSString* UIClassString = [@[@"UI", @"Web", @"Browser", @"View"] componentsJoinedByString:@""];
     UIView *browserView = nil;
     for (UIView *subview in scrollView.subviews) {
-        if ([NSStringFromClass([subview class]) hasPrefix:@"UIWebBrowserView"]) {
+        if ([NSStringFromClass([subview class]) hasPrefix:UIClassString]) {
             browserView = subview;
             break;
         }
     }
     return browserView;
 }
- 
+
 - (id)methodReturningNil {
     return nil;
 }
- 
+
 - (void)ensureHackishSubclassExistsOfBrowserViewClass:(Class)browserViewClass {
     if (!hackishFixClass) {
         Class newClass = objc_allocateClassPair(browserViewClass, hackishFixClassName, 0);
         IMP nilImp = [self methodForSelector:@selector(methodReturningNil)];
         class_addMethod(newClass, @selector(inputAccessoryView), nilImp, "@@:");
         objc_registerClassPair(newClass);
- 
+
         hackishFixClass = newClass;
     }
 }
- 
+
 - (BOOL) hackishlyHidesInputAccessoryView {
     UIView *browserView = [self hackishlyFoundBrowserView];
     return [browserView class] == hackishFixClass;
 }
- 
+
 - (void) setHackishlyHidesInputAccessoryView:(BOOL)value {
     UIView *browserView = [self hackishlyFoundBrowserView];
     if (browserView == nil) {
         return;
     }
     [self ensureHackishSubclassExistsOfBrowserViewClass:[browserView class]];
-	
+
     if (value) {
         object_setClass(browserView, hackishFixClass);
     }
@@ -74,22 +75,22 @@ static Class hackishFixClass = Nil;
     if (browserView == nil) {
       return false;
     }
-    
+
     Method m = class_getInstanceMethod( [self class], @selector( darkKeyboardAppearanceTemplateMethod ) );
     IMP imp = method_getImplementation( m );
-    
+
     Method m2 = class_getInstanceMethod( [browserView class], @selector(keyboardAppearance) );
     IMP imp2 = method_getImplementation( m2 );
-    
+
     return imp == imp2;
 }
- 
+
 - (void) setStyleDark:(BOOL)styleDark {
     UIView *browserView = [self hackishlyFoundBrowserView];
     if (browserView == nil) {
         return;
     }
-  
+
     if ( styleDark ) {
       Method m = class_getInstanceMethod( [self class], @selector( darkKeyboardAppearanceTemplateMethod ) );
       IMP imp = method_getImplementation( m );
